5d1f4eeb00ba97a2c5a91413dce697131deba73f
Morshed <skmorshed75@yahoo.com>
2024-01-01 23:19:34 +0600

    Second phase User Registration Completed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,18 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     function UserRegistration(Request $request){
-       return User::create([
-            'firstName' => $request->input('firstName'),
-            'lastName' => $request->input('lastName'),
-            'email' => $request->input('email'),
-            'mobile' => $request->input('mobile'),
-            'password' => $request->input('password')
-        ]);
+        try{
+            User::create([
+                'firstName' => $request->input('firstName'),
+                'lastName' => $request->input('lastName'),
+                'email' => $request->input('email'),
+                'mobile' => $request->input('mobile'),
+                'password' => $request->input('password')
+            ]);
+    
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User Registration Successful'
+            ],200);
+        }
+        catch(Exception $e){
+            return response()->json([
+                'status' => 'fail',
+                //'message' => 'User Registration Failed'
+                'message' => $e->getMessage()
+            ],400);
+        };
     }
 }
